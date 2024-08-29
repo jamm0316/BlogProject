6fee3d21e095ad9f180dde103e78e8dc0c511d39
evan <168372878+jamm0316@users.noreply.github.com>
2024-08-29 22:26:18 +0900

    Update TestController.java

--- src/main/java/springboot/TestController.java
+++ src/main/java/springboot/TestController.java
@@ -1,26 +1,22 @@
 package me.evansong.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestController {
-    @GetMapping("/")
-    public String home() {
-        return "home";
-    }
 
+    @Autowired  //TestService 빈 주입
+    TestService testService;
 
+    // 회원 정보 조회
     @GetMapping("/test")
-    public String test() {
-        String str = """
-                hello,
-                world
-                my name
-                is
-                evan
-                """;
-        System.out.println(str);
-        return str;
+    public List<Member> getAllMembers() {
+        List<Member> members = testService.getAllMembers();
+        return members;  //사용자에게 보여줄 것은 List member
     }
+
 }
